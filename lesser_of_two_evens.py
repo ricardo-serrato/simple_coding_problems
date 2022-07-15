e2f4713478348d3f27babace47815f5df632fe0f
Ricardo Serrato <serrato.ricardo97@gmail.com>
2022-07-15 17:15:01 -0500

    problem and solution

---
+++ lesser_of_two_evens.py
@@ -0,0 +1,29 @@
+"""
+WARMUP SECTION:
+LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns
+-the greater if one or both numbers are odd
+lesser_of_two_evens(2,4) --> 2
+lesser_of_two_evens(2,5) --> 5
+"""
+
+
+def lesser_of_two_evens(a,b):
+    if a % 2 == 0 and b % 2 == 0:
+        return a if a < b else b
+    else:
+        return a if a > b else b
+
+
+print(lesser_of_two_evens(2, 2))
+print(lesser_of_two_evens(5, 5))
+
+
+def lesser_of_two_evens(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        return min(a, b)
+    else:
+        return max(a, b)
+
+
+print(lesser_of_two_evens(2, 2))
+print(lesser_of_two_evens(5, 5))
