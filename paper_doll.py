d608f92bca1ade965a60df964c55635b042a6025
Ricardo Serrato <serrato.ricardo97@gmail.com>
2022-07-15 17:20:10 -0500

    problem and solution

---
+++ paper_doll.py
@@ -0,0 +1,18 @@
+"""
+ PAPER DOLL: Given a string, return a string where for every character in the original there are three characters¶
+ paper_doll('Hello') --> 'HHHeeellllllooo'
+ paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
+"""
+
+
+def paper_doll(text):
+    new_string = ''
+
+    for char in text:
+        new_string += char*3
+
+    return new_string
+
+
+
+print(paper_doll('Mississippi'))
